d14d06e0ecd54427f014840b1dbcb6e0515a0520
BuildTools <CynthiaWang7979@hotmail.com>
2021-01-23 15:28:32 +0800

    Remove disused code

--- main.py
+++ main.py
@@ -84,47 +84,11 @@ def show_main_gui(frame, width, height):
             (BUTTON_MARGIN+TEXT_MARGIN, BUTTON_MARGIN+35-TEXT_MARGIN + (BUTTON_MARGIN+35)),
             FONT, fontScale=1, color=BLACK, thickness=2, lineType=LINE_AA)
 
-    # frame = draw_labelled_button(frame, button_w=147, button_h=35,
-    #                              window_w=width, window_h=height,
-    #                              text='Hide GUI')
-    # frame = draw_labelled_button(frame, button_w=200, button_h=35,
-    #                              window_w=width, window_h=height,
-    #                              align="topleft",
-    #                              text='Add Accessory')
-    # frame = draw_labelled_button(frame, button_w=250, button_h=35,
-    #                              window_w=width, window_h=height,
-    #                              align="topleft",
-    #                              text='Set/Remove Still Frame')
     imshow(MAIN_WINDOW_NAME, frame)
+
 
 def mouse_callback(event, x, y, flag, *userdata):
     print(event, x, y, flag, *userdata)
-
-
-# def draw_labelled_button(surface, button_w, button_h, window_w, window_h, text, color=(255, 255, 255), align='topright',
-#                          margin=10, callback=lambda *_: None):
-#     # Draw the Rectangle
-#     left_x = window_w-margin - button_w if 'right' in align else margin
-#     top_y = globals()['next_'+align]+margin if 'top' in align else globals()['next_'+align]-margin - button_h
-#     right_x = window_w-margin if 'right' in align else margin + button_w
-#     bottom_y = globals()['next_'+align]+margin + button_h if 'top' in align else globals()['next_'+align]-margin
-#     surface = rectangle(surface, (left_x, top_y), (right_x, bottom_y), color, thickness=-1)
-#
-#     # Draw the Text
-#     putText(surface, text, (left_x + 2, bottom_y - 5), FONT, 1, color=(0, 0, 0), thickness=2, lineType=LINE_AA)
-#
-#     # Update next positions
-#     print(globals()['next_'+align], align)
-#     if text not in globals()['buttons']:  # New button
-#         if 'top' in align:
-#             globals()['next_'+align] += button_h
-#             print('changed:', globals()['next_' + align], align)
-#         else:
-#             globals()['next_'+align] -= button_h
-#         globals()['buttons'].append(text)
-#         globals()['callbacks'][((left_x, top_y), (right_x, bottom_y))] = callback
-#
-#     return surface
 
 
 if __name__ == '__main__':
